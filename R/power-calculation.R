a40b9ce99339f0451d3359aae0022df5a9fc434f
KaiWang <kai.wang@usys.ethz.ch>
2025-01-17 20:17:22 +0100

    Upgrade: Correlation Structures, Easier Usage

--- R/power-calculation.R
+++ R/power-calculation.R
@@ -1,131 +1,306 @@
-#' Power of omnibus test
+#' Power of omnibus tests
 #'
 #' Calculate power for testing overall effects of treatment factors and their
-#' interactions, i.e., statistical power of ANOVA.
+#' interactions, i.e., statistical power of F-test.
 #'
-#' @param design a design object created using design generating functions.
-#' @param alpha significance level (type I error rate), default 0.05
-#' @param ... Additional arguments passed to \link[lmerTest]{anova.lmerModLmerTest}
-#' for linear mixed models and to \link[car]{Anova} for linear models. The type
-#' of sum of squares (SS, default is Type III) and the method for computing denominator
-#' degrees of freedom (DDF, default is Satterthwaite's method) can be modified.
-#' For balanced designs, types of SS and DDF do not affect results. Note that
-#' these additional arguments should be consistent in the design-generating
-#' function and \code{pwr.anova} for linear mixed models.
+#' @param object a design object created in pwr4exp
+#' @param sig.level significance level, default 0.05
+#' @param type the type of ANOVA table requested, default Type III
 #'
 #' @return a data frame with numerator degrees of freedom (NumDF), denominator
-#' degrees of freedom (DenDF), non-centrality parameter, type I error rate (alpha),
-#' and power.
+#' degrees of freedom (DenDF), type I error rate (sig.level), and power.
 #'
-#' @seealso [designCRD()], [designRCBD()], [designLSD()], [designCOD()], [designSPD()], [designCustom()], and [pwr.contrast()]
+#' @seealso [mkdesign()], [designCRD()], [designRCBD()], [designLSD()], [designCOD()], [designSPD()], [pwr.summary()] and [pwr.contrast()]
 #' @export
 #' @examples
 #' # generate an RCBD
-#' rcbd = designRCBD(treatments = c(2, 2), blocks = 10, beta = c(10, 9, 8, 7), VarCov = 10, sigma2 = 9)
+#' rcbd <- designRCBD(
+#'   treatments = c(2, 2),
+#'   label = list(facA = c("1", "2"), facB = c("1", "2")),
+#'   blocks = 12,
+#'   formula = ~ facA*facB + (1|block),
+#'   means = c(32, 35, 30, 37),
+#'   vcomp = 4,
+#'   sigma2 = 6
+#' )
 #' # power of omnibus test
-#' pwr.anova(rcbd, alpha  = 0.05)
-pwr.anova <- function(design, alpha = 0.05, ...) {
-  UseMethod("pwr.anova", design)
-}
-
-#' @exportS3Method pwr.anova lmmDesign
-pwr.anova.lmmDesign <- function(design, alpha = 0.05, ...){
-  args <- list(...)
-  if (!("type" %in% names(args))) {
-    args$type <- "3"
+#' pwr.anova(rcbd)
+#'
+#' @importFrom utils  as.roman
+pwr.anova <- function(object,
+                      sig.level = 0.05,
+                      type = c("III", "II", "I", "3", "2", "1")) {
+  type <- type[1L]
+  if(!is.character(type)) type <- as.character(type)
+  type <- match.arg(type)
+  if(type %in% c("I", "II", "III"))
+    type <- as.character(as.integer(as.roman(type)))
+  # Get list of contrast matrices (L) - one for each model term:
+  L_list <- if(type == "1") {
+    get_contrasts_type1(object)
+  } else if(type == "2") {
+    get_contrasts_type2_unfolded(object)
+  } else if(type == "2b") {
+    get_contrasts_type2(object)
+  } else if(type == "3") {
+    get_contrasts_type3(object)
+  } else if(type == "yates") {
+    get_contrasts_yates(object)
+  } else if(type == "marginal") {
+    get_contrasts_marginal(object)
+  } else {
+    stop("'type' not recognized")
   }
-  if (!("ddf" %in% names(args))) {
-    args$ddf <- "Satterthwaite"
+  # Get power for each term and collect in table:
+  table <- do.call(rbind, lapply(L_list, function(L) contestMD(object, L, sig.level)))
+  # Format power table and return:
+  if(length(nm <- setdiff(names(L_list), rownames(table)))) {
+    tab <- array(NA_real_, dim=c(length(nm), 6L),
+                 dimnames = list(nm, colnames(table)))
+    table <- rbind(table, tab)
   }
-  res <- as.data.frame(do.call(stats::anova, c(list(design$pseu_model), args)))
-  res$DenDF <- design$pseu_model@DenDF
-  res$`non-centrality` <- res$`F value` * res$NumDF
-  res$Fcrt <- stats::qf(p = alpha, df1 = res$NumDF, df2 = res$DenDF, lower.tail = FALSE)
-  res$power <- 1 - stats::pf(q = res$Fcrt, df1 = res$NumDF, df2 = res$DenDF, ncp = res$`non-centrality`)
-  res <- res[, c("NumDF", "DenDF", "non-centrality", "power")]
-  res$alpha <- alpha
-  res <- res[, c("NumDF", "DenDF", "non-centrality", "alpha", "power")]
-  attr(res, "heading") <- paste("Power Analysis of", design$design)
-  class(res) <- c("anova", "data.frame")
-  return(res)
-}
-
-#' @exportS3Method pwr.anova lmDesign
-pwr.anova.lmDesign <- function(design, alpha = 0.05, ...){
-  formula <- design$formula
-  # Change contrast coding to contr.sum in lm
-  data2 <- design$pseu_model$model
-  for (factor_name in names(data2)) {
-    if (is.factor(data2[[factor_name]])) {
-      stats::contrasts(data2[[factor_name]]) <- stats::contr.sum
-    }
-  }
-  design$pseu_model <- stats::update(design$pseu_model, data = data2)
-  args <- list(...)
-  if (!("type" %in% names(args))) {
-    args$type <- "3"
-  }
-  res <- as.data.frame(do.call(car::Anova, c(list(design$pseu_model), args)))
-  res$NumDF <- res$Df
-  res$DenDF <- design$pseu_model$df.residual
-  res$`non-centrality` <- res$`F value` * res$NumDF
-  res$Fcrt <- stats::qf(p = alpha, df1 = res$NumDF, df2 = res$DenDF, lower.tail = FALSE)
-  res$power <- 1 - stats::pf(q = res$Fcrt, df1 = res$NumDF, df2 = res$DenDF, ncp = res$`non-centrality`)
-  res$alpha <- alpha
-  res <- res[, c("NumDF", "DenDF", "non-centrality", "alpha", "power")]
-  res <- res[!rownames(res) %in% c("(Intercept)", "Residuals"), ]
-  attr(res, "heading") <- paste("Power analysis of", design$design)
-  class(res) <- c("anova", "data.frame")
-  return(res)
+  # Format 'type':
+  type <- if(type == "marginal") {
+    "Marginal"
+  } else if (type == "yates" || type == "3b") {
+    "Yates"
+  } else if(grepl("b|c", type)) {
+    alph <- gsub("[0-9]", "", type)
+    paste0("Type ", as.roman(as.integer(gsub("b|c", "", type))), alph)
+  } else paste("type", as.roman(as.integer(type)))
+  attr(table, "heading") <-
+    paste("Power of", type, "F-test")
+  attr(table, "hypotheses") <- L_list
+  class(table) <- c("anova", "data.frame")
+  table
 }
 
 #' Power of contrasts
 #'
-#' Calculate power for testing various contrasts. The same syntax of
-#' \link[emmeans:emmeans-package]{emmeans} package is employed to specify contrast types.
+#' Calculate power for testing various contrasts.
 #'
-#' @param design a design object created using design generating functions.
-#' @param specs an argument inherited from \link[emmeans]{emmeans} specifying
-#' the names of the factors over which the contrasts are performed.
-#' @param method an argument inherited from \link[emmeans]{contrast} specifying
-#' the method of contrasts, e.g., pairwise, linear, and polynomials.
-#' @param alpha significance level (type I error rate), default 0.05
-#' @param ... other arguments passed to \link[emmeans]{contrast}.
+#' @param object design object created in pwr4exp
+#' @param which the factor of interest
+#' @param by the variable to condition on
+#' @param contrast contrast method, include "pairwise", "poly", and "trt.vs.ctrl",
+#' or any manually defined contrast vector
+#' @param sig.level significance level, default 0.05
+#' @param p.adj whether the sig.level should be adjusted using the Bonferroni method, default FALSE
+#' @param alternative one- or two-sided test. Can be abbreviated.
+#' @param strict use strict interpretation in two-sided case
+#'
 #' @export
-#' @return a data frame showing the power of the specific contrast
+#' @return a data frame or a list of data frame separated by conditions.
 #' @examples
-#' rcbd = designRCBD(treatments = c(2, 2), blocks = 10, beta = c(10, 9, 8, 7), VarCov = 10, sigma2 = 9)
-#' pwr.contrast(rcbd, specs = ~ facA|facB, method = "pairwise")
-pwr.contrast <- function(design, specs, method, alpha = 0.05, ...){
-  args <- list(...)
-  if (!("lmer.df" %in% names(args))) {
-    args$lmer.df <- "kenward-roger"
+#' rcbd <- designRCBD(
+#'   treatments = c(2, 2),
+#'   label = list(facA = c("1", "2"), facB = c("1", "2")),
+#'   blocks = 12,
+#'   formula = ~ facA*facB + (1|block),
+#'   means = c(32, 35, 30, 37),
+#'   vcomp = 4,
+#'   sigma2 = 6
+#' )
+#' pwr.contrast(rcbd, which = "facA", by = "facB")
+pwr.contrast <- function(object,
+                         which,
+                         by = NULL,
+                         contrast = c("pairwise", "poly", "trt.vs.ctrl"),
+                         sig.level = 0.05,
+                         p.adj = FALSE,
+                         alternative = c("two.sided", "one.sided"),
+                         strict = TRUE) {
+
+  contrast <- match.arg(contrast)
+  alternative <- match.arg(alternative)
+
+  ## FIXME: check estimability?
+  L_means <- lsmeans_contrasts(object, which, by  = by)
+
+  if (is.null(by)) {
+    if (contrast == "pairwise") cl <- emmeans::pairwise.emmc(rownames(L_means))
+    if (contrast == "poly") cl <- emmeans::poly.emmc(rownames(L_means), max.degree = 3)
+    if (contrast == "trt.vs.ctrl") cl <- emmeans::trt.vs.ctrl.emmc(rownames(L_means))
+    if (is.numeric(contrast) & is.vector(contrast)) cl <- matrix(cl, nrow = length(cl))
+    if (is.matrix(contrast) | is.data.frame(contrast)) cl <- contrast
+    L <- crossprod(as.matrix(cl), L_means)
+    if (p.adj) sig.level <- sig.level/nrow(L)
+    tab <- apply(L, 1, function(l){
+      contest1D(object = object, L = l, sig.level = sig.level, alternative = alternative, strict = strict)
+    })
+    tab <- do.call(rbind, tab)
+    return(tab)
   }
-  if (inherits(design, "lmmDesign")) {
-    emm <- do.call(
-      emmeans::emmeans,
-      c(list(object = design$pseu_model, specs = specs), args)
-    )
-  } else if (inherits(design, "lmDesign")) {
-    emm <- do.call(
-      emmeans::emmeans,
-      c(list(object = design$pseu_model, specs = specs), args)
-    )
-  } else {
-    stop("design is neither an lmmDesign nor an lmDesign object.")
+
+  if (!is.null(by)) {
+    tab_list <- lapply(L_means, function(l_means){
+      if (contrast == "pairwise") cl <- emmeans::pairwise.emmc(rownames(l_means))
+      if (contrast == "poly") cl <- emmeans::poly.emmc(rownames(l_means), max.degree = 3)
+      if (contrast == "trt.vs.ctrl") cl <- emmeans::trt.vs.ctrl.emmc(rownames(l_means))
+      if (is.numeric(contrast) & is.vector(contrast)) cl <- matrix(cl, nrow = length(cl))
+      if (is.matrix(contrast) | is.data.frame(contrast)) cl <- contrast
+      L <- crossprod(as.matrix(cl), l_means)
+      if (p.adj) sig.level <- sig.level/nrow(L)
+      tab <- apply(L, 1, function(l){
+        contest1D(object = object, L = l, sig.level = sig.level, alternative = alternative)
+      })
+      tab <- do.call(rbind, tab)
+      return(tab)
+    })
+    by_names <- names(tab_list)
+    list_names <- gsub(paste0("^", by), paste0(by, " = "), by_names)
+    names(tab_list) <- list_names
+    return(tab_list)
   }
-  emm.contrast <- as.data.frame(emmeans::contrast(emm, method = method))
-  emm.contrast$f.ratio <- emm.contrast$t.ratio^2
-  emm.contrast$df <- round(emm.contrast$df)
-  emm.contrast$`non-centrality` <- emm.contrast$f.ratio
-  emm.contrast$Fcrt <- stats::qf(p = alpha, df1 = 1, df2 = emm.contrast$df, lower.tail = FALSE)
-  emm.contrast$alpha <- alpha
-  emm.contrast$power <- 1 - stats::pf(
-    q = emm.contrast$Fcrt,
-    df1 = 1,
-    df2 = emm.contrast$df,
-    ncp = emm.contrast$`non-centrality`
-  )
-  emm.contrast <- emm.contrast[, !names(emm.contrast) %in% c("SE", "t.ratio", "p.value", "f.ratio", "Fcrt")]
-  return(emm.contrast)
+}
+
+
+#' power of model coefficients
+#'
+#' @param object a design object created in pwr4exp
+#' @param sig.level significance level, default 0.05
+#'
+#' @return power table for each model coefficient
+#' @export
+#'
+#' @examples
+#' rcbd <- designRCBD(
+#'   treatments = c(2, 2),
+#'   label = list(facA = c("1", "2"), facB = c("1", "2")),
+#'   blocks = 12,
+#'   formula = ~ facA*facB + (1|block),
+#'   means = c(32, 35, 30, 37),
+#'   vcomp = 4,
+#'   sigma2 = 6
+#' )
+#' pwr.summary(rcbd)
+pwr.summary <- function(object, sig.level = 0.05) {
+  X <- object$X
+  p <- ncol(X)
+  if(p < 1)
+    return(as.matrix(contest1D(object, numeric(0L))))
+  Lmat <- diag(p)
+  tab <- do.call(rbind, lapply(1:p, function(i) contest1D(object, Lmat[i, ])))
+  rownames(tab) <- colnames(X)
+  tab
+}
+
+#' @noRd
+#' @import stats
+contest1D <- function(object,
+                      L,
+                      method=c("Satterthwaite"),
+                      sig.level = 0.05,
+                      alternative = c("two.sided", "one.sided"),
+                      strict = TRUE) {
+
+  mk_ttable <- function(effect, se, df, sig.level, alternative, strict = TRUE) {
+    tside <- switch(alternative, one.sided = 1, two.sided = 2)
+    ncp <- abs(effect/se)
+
+    if (strict && tside == 2) {
+      tvalue <- qt(sig.level/tside, df, lower.tail = FALSE)
+      power <- pt(tvalue, df, ncp = ncp, lower.tail = FALSE) +
+        pt(-tvalue, df, ncp = ncp, lower.tail = TRUE)
+    } else {
+      tvalue <- qt(sig.level/tside, df, lower.tail = FALSE)
+      power <- pt(tvalue, df, ncp = ncp, lower.tail = FALSE)
+    }
+
+    data.frame("effect"=effect, "df"=df,
+               "sig.level"=sig.level,
+               "power"=power, alternative=alternative,
+               check.names=FALSE)
+  }
+  if(is.matrix(L)) L <- drop(L) # L is a column vector
+  stopifnot(is.numeric(L), length(L) == length(object$beta))
+  effect <- sum(L * object$beta) # contrast effect (expectation)
+  method <- match.arg(method) # currently, just one option
+  alternative <- match.arg(alternative)
+  if(method == "Satterthwaite") {
+    var_con <- sum(L * (object$vcov_beta %*% L)) # variance of contrast
+    # Compute denominator DF:
+    grad_var_con <-
+      vapply(object$Jac_list, function(J) sum(L * (J %*% L)), numeric(1L)) # = {L' Jac L}_i
+    satt_denom <- sum(grad_var_con * (object$vcov_varpar %*% grad_var_con)) # g'Ag
+    df <- drop(2 * var_con^2 / satt_denom) # denominator DF
+  } # TODO: other DF methods
+  mk_ttable(effect, sqrt(var_con), df, sig.level, alternative)
+}
+
+#' @noRd
+#' @import stats
+contestMD <- function(object, L,
+                      sig.level = 0.05,
+                      eps=sqrt(.Machine$double.eps),
+                      ...) {
+
+  get_Fstat_ddf <- function(nu, tol=1e-8) {
+    fun <- function(nu) {
+      if(any(nu <= 2)) 2 else {
+        E <- sum(nu / (nu - 2))
+        2 * E / (E - (length(nu))) # q = length(nu) : number of t-statistics
+      }
+    }
+    stopifnot(length(nu) >= 1,
+              # all(nu > 0), # returns 2 if any(nu < 2)
+              all(sapply(nu, is.numeric)))
+    if(length(nu) == 1L) return(nu)
+    if(all(abs(diff(nu)) < tol)) return(mean(nu))
+    if(!is.list(nu)) fun(nu) else vapply(nu, fun, numeric(1L))
+  }
+  mk_Ftable <- function(ndf, ddf, ncp, sig.level) {
+    Fvalue <- stats::qf(sig.level, ndf, ddf, lower.tail = FALSE)
+    power <- stats::pf(Fvalue, ndf, ddf, ncp, lower.tail = FALSE)
+
+    data.frame("NumDF"=ndf, "DenDF"=ddf,
+               "sig.level"=sig.level,
+               "power"=power,
+               check.names=FALSE)
+  }
+  if(!is.matrix(L)) L <- matrix(L, ncol=length(L))
+  stopifnot(is.matrix(L), is.numeric(L),
+            ncol(L) == length(object$beta))
+  if(nrow(L) == 0L) { # May happen if there are no fixed effects
+    x <- numeric(0L)
+    return(mk_Ftable(x, x, x, x))
+  }
+  beta <- object$beta
+  vcov_con <- L %*% object$vcov_beta %*% t(L) # (co)-variance of contrasts
+  eig_vcov_con <- eigen(vcov_con)
+  P <- eig_vcov_con$vectors
+  d <- eig_vcov_con$values
+  tol <- max(eps * d[1], 0)
+  pos <- d > tol
+  q <- sum(pos) # rank(vcov_con)
+  if(q < nrow(L))
+    warning("Contrast is rank deficient and test may be affected")
+  # FIXME: matrix, vector, 1D case
+  PtL <- crossprod(P, L)[1:q, , drop = FALSE]
+  # Compute q-list of gradients of (PtL)' cov(beta) (PtL) wrt. varpar vector
+  grad_PLcov <- lapply(1:q, function(m) {
+    vapply(object$Jac_list, function(J) PtL[m, , drop = FALSE] %*% J %*% t(PtL[m, , drop = FALSE]), numeric(1L))
+  })
+  # Compute degrees of freedom for the q t-statistics:
+  nu_m <- vapply(1:q, function(m) {
+    2*(d[m])^2 / sum(grad_PLcov[[m]] * (object$vcov_varpar %*% grad_PLcov[[m]])) }, numeric(1L)) # 2D_m^2 / g'Ag
+  # Compute ddf for the F-value:
+  ddf <- get_Fstat_ddf(nu_m, tol=1e-8)
+
+  # FIXME; two NCP approaches should give the same value
+  # get_ncp <- function(L, beta, X, Z, G, R){
+  #   if (is.null(Z) | is.null(G)) {
+  #     V <- R
+  #   } else {
+  #     V <- Z %*% G %*% Matrix::t(Z) + R
+  #   }
+  #   C = solve(t(X) %*% Matrix::solve(V) %*% X)
+  #   ncp = drop(t(L %*% beta) %*% solve(L %*% C %*% t(L)) %*% (L %*% beta))
+  #   return(ncp)
+  # }
+  #
+  # ncp <- get_ncp(L, beta, object$X, Matrix::object$reTrms$Zt, object$reTrms$G, object$rTrms$R)
+  t2 <- drop(PtL %*% beta)^2 / d[1:q]
+  ncp <- sum(t2)
+  mk_Ftable(ndf = q, ddf = ddf, ncp = ncp, sig.level = sig.level)
 }
--- README.md
+++ README.md
@@ -1,57 +1,75 @@
-
 # pwr4exp
 
-The `pwr4exp` R package provides functions for power calculation and sample size determination in standard experimental designs in animal science and beyond. The package emphasizes the importance of specifying models for conducting power analyses and supports power analyses for main effects, interactions, and specific contrasts. Additionally, `pwr4exp` offers a flexible framework to perform power analysis on customized designs which are currently not predefined in the package.
+The `pwr4exp` R package provides functions for power calculation and sample size determination in standard experimental designs in animal science and beyond. The package emphasizes the importance of specifying models for conducting power analyses and supports power analyses for main effects, interactions, and specific contrasts. Additionally, `pwr4exp` offers a flexible framework to perform power analysis on customized designs which are currently not predefined in the package. The development version incorporates various correlation structures for repeated measures and spatial data directly from the `nlme` package.
 
 <!-- badges: start -->
+
 <!-- badges: end -->
 
 ## Installation
 
-```r
+``` r
 # You can install pwr4exp from CRAN
 install.packages("pwr4exp")
 
-# Or the the development version from GitHub:
+# Or the development version from GitHub:
 # install.packages("devtools")
 devtools::install_github("an-ethz/pwr4exp")
 ```
 
 ## Functions
 
-Performing power analysis in `pwr4exp` involves the following steps:
-- First, create a design object using the design generating functions.
-- Once the design object is created, calculating power or determining sample size using `pwr4exp` is straightforward. Simply pass the design object to the power calculator for main effects and interactions, `pwr.anova()`, or for contrasts, `pwr.contrast()`.
-- To determine the minimal sample size to achieve a target power, a quoted design object can be passed to the function `find_sample_size()`.
+Performing power analysis in `pwr4exp` involves the following steps: - First, create a design object using the design generating functions. - Once the design object is created, calculating power or determining sample size using `pwr4exp` is straightforward. Simply pass the design object to the power calculator for main effects and interactions, `pwr.anova()`, or for contrasts, `pwr.contrast()`.
 
 ### Example Usage
 
 Here's an example of how you can generate a design and calculate power:
 
-```r
+``` r
 library(pwr4exp)
-# Create a design
-rcbd <- designRCBD(
-  treatments = c(2, 2),
-  blocks = 10,
-  beta = c(470, 30, -55, 5),
-  VarCov = 3200,
-  sigma2 = 3200
-)
-# Calculate power for the ominubus test (i.e., F-test)
-pwr.anova(design = rcbd)
 
-# Calculate power for specific contrasts
-pwr.contrast(design = rcbd, specs = ~ facB | facA, contrast = "pairwise")
+## representing a CRD with repeated measures at 8 time points
+
+n_subject = 6
+n_trt = 3
+n_hour = 8
+trt = c("CON", "TRT1", "TRT2")
+
+df.rep <- data.frame(
+  subject = as.factor(rep(seq_len(n_trt*n_subject), each = n_hour)),
+  hour = as.factor(rep(seq_len(n_hour), n_subject*n_trt)),
+  trt = rep(trt, each = n_subject*n_hour)
+)
+
+# templates
+mkdesign(formula = ~ trt*hour, data = df.rep)
+
+# create the design
+
+design.rep <- mkdesign(
+formula = ~ trt*hour,
+data = df.rep,
+means =  c(1, 2.50, 3.5,
+           1, 3.50, 4.54,
+           1, 3.98, 5.80,
+           1, 4.03, 5.4,
+           1, 3.68, 5.49,
+           1, 3.35, 4.71,
+           1, 3.02, 4.08,
+           1, 2.94, 3.78),
+sigma2 = 2,
+correlation = corAR1(value = 0.6, form = ~ hour|subject)
+)
+
+pwr.anova(design.rep)
 ```
 
 ## Learn More
 
-To learn more about `pwr4exp`, read through the [vignette](https://an-ethz.github.io/pwr4exp/articles/pwr4exp.html) for `pwr4exp` which contains:
+The page will be updated recently to reflect the newly add functions.
 
-- Foundational Concepts.
-- Instructions for providing the required inputs.
-- Examples of conducting power analysis for the standard designs available in the package.
-- Examples of using `pwr4exp` to assess the power of customized designs.
+~~To learn more about `pwr4exp`, read through the [vignette](https://an-ethz.github.io/pwr4exp/articles/pwr4exp.html) for `pwr4exp` which contains:~~
+
+~~- Foundational Concepts.~~ ~~- Instructions for providing the required inputs.~~ \~\~- Examples of conducting power analysis for the standard designs available in the package. ~~- Examples of using `pwr4exp` to assess the power of customized designs.~~
 
 The documentation for this package is being updated. If you have any questions or suggestions, please feel free to contact the package maintainer.
